77a016c2acffdc34a45c96bbba21c5fb8645b17e
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-03 13:57:47 -0500

    ip fix

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ app = FastAPI()
 app.include_router(image.router, prefix="/image")
 app.include_router(video.router, prefix="/video")
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+
 if __name__ == "__main__":
     import uvicorn
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    
+    local_ip = get_local_ip()
     print(f"App is accessible at: http://{local_ip}:8000")
     uvicorn.run(app, host="0.0.0.0", port=8000)
