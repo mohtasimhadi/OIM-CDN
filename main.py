c68585f044de9be9a045668d132bc35de396132a
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-03 13:50:31 -0500

    accessible from local network

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import socket
 from fastapi import FastAPI
-from routers import image
-from routers import video
+from routers import image, video
 
 app = FastAPI()
 app.include_router(image.router, prefix="/image")
 app.include_router(video.router, prefix="/video")
+
+if __name__ == "__main__":
+    import uvicorn
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    
+    print(f"App is accessible at: http://{local_ip}:8000")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
